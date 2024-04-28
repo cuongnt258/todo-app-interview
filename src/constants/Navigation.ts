1fd427b3a9919c0edb59432bfb4d83080a58a136
Cuong Nguyen <cuongnt258@gmail.com>
2024-04-28 12:54:11 +0700

    chore: Define navigation type

---
+++ src/constants/Navigation.ts
@@ -0,0 +1,27 @@
+import {NavigatorScreenParams, PathConfigMap} from '@react-navigation/native';
+
+export const SCREENS = {} as const satisfies {
+  [key: string]: React.ComponentType<{}> & {
+    title: string;
+    linking: object | undefined;
+    options?: object;
+  };
+};
+
+export type MainStackParamList = {
+  Dashboard: undefined;
+  Todo: undefined;
+};
+
+type ExampleScreensParamList = {
+  [Key in keyof typeof SCREENS]: (typeof SCREENS)[Key]['linking'] extends PathConfigMap<
+    infer P
+  >
+    ? NavigatorScreenParams<P> | undefined
+    : undefined;
+};
+
+export type RootStackParamList = ExampleScreensParamList & {
+  Auth: undefined;
+  Main: NavigatorScreenParams<MainStackParamList> | undefined;
+};
---
+++ src/types/global.d.ts
@@ -0,0 +1,8 @@
+import {RootStackParamList} from '../constants/Navigation';
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace ReactNavigation {
+    interface RootParamList extends RootStackParamList {}
+  }
+}
