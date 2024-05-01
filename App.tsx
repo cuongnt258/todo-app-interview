8ef8badbe979eab6bc79ed1ce6ac8d6aa15e0fb1
Cuong Nguyen <cuongnt258@gmail.com>
2024-05-01 23:39:26 +0700

    Chore: update app

--- App.tsx
+++ App.tsx
@@ -5,7 +5,14 @@
  * @format
  */
 import React from "react";
+import "react-native-gesture-handler";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import Toast from "react-native-toast-message";
 import { Provider } from "react-redux";
+
+import Loading from "@components/common/Loading";
+
+import { RXStoreComponent } from "@store/actions";
 
 import { NavigationContainer, RootStack } from "./src/navigation";
 import store from "./src/store";
@@ -13,9 +20,14 @@ import store from "./src/store";
 function App(): React.JSX.Element {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <RootStack />
-      </NavigationContainer>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <RootStack />
+          <Loading />
+          <RXStoreComponent />
+          <Toast />
+        </NavigationContainer>
+      </SafeAreaProvider>
     </Provider>
   );
 }
